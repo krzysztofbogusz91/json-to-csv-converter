75a75830994287be3051446d543a68731abb8dac
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2019-02-15 15:09:59 +0100

    refactored

--- src/id-populator.module.js
+++ src/id-populator.module.js
@@ -1,17 +1,20 @@
 const _ = require('lodash');
-const setId = (elem, id) =>{
+const setId = (elem, id) => {
   if (elem.hasOwnProperty('idFirmy')) {
     return elem.idFirmy;
-  }else {
-    return id
+  } else {
+    return id;
   }
-}
+};
 
-const deleteArrayElement = (companyOrMember, settings) =>{
+const deleteArrayElement = (companyOrMember, settings) => {
   return companyOrMember.map(elem => {
     const arrayField = Object.keys(elem).filter(arrayObj => {
       if (Array.isArray(elem[arrayObj])) {
-        populateCompanyIds(elem[arrayObj],{ currentTable: settings.currentTable, idPres: setId(elem, settings.idPres)});
+        populateCompanyIds(elem[arrayObj], {
+          currentTable: settings.currentTable,
+          idPres: setId(elem, settings.idPres)
+        });
         return true;
       }
       return false;
@@ -19,26 +22,23 @@ const deleteArrayElement = (companyOrMember, settings) =>{
 
     return _.omit(elem, arrayField[0]);
   });
-}
-const populateCompanyIds = (entityToPopulate, settings ={
-  currentTable: [],
-  idPres: 0
-} 
- ) => {
-
-  const filtered = deleteArrayElement(entityToPopulate, settings)
-
-  filtered.forEach(elem => {
-    if (!elem.hasOwnProperty('idFirmy')) {
-      settings.currentTable.push({ idFirmy: setId(elem, settings.idPres), ...elem });
-    } else {
-      settings.currentTable.push(elem);
-    }
-  });
-
+};
+const populateCompanyIds = (
+  entityToPopulate,
+  settings = {
+    currentTable: [],
+    idPres: 0
+  }
+) => {
+  deleteArrayElement(entityToPopulate, settings).forEach(elem =>
+    !elem.hasOwnProperty('idFirmy')
+      ? settings.currentTable.push({
+          idFirmy: setId(elem, settings.idPres),
+          ...elem
+        })
+      : settings.currentTable.push(elem)
+  );
   return settings.currentTable;
 };
 
-
 module.exports = { populateCompanyIds };
-
